955bed7d5fb5b475771f2d4ddbdc7234c1d85777
Apruzzese Francesco <cescoap@gmail.com>
2012-05-10 18:39:39 +0200

    [ADD] Info PHP file

---
+++ info.php
@@ -0,0 +1,14 @@
+<?php
+
+	$SOFTWARE_DATA = array(
+		'Version' => '1.0.0',
+		'StableVersion' => 'Yes',
+		'Developer' => 'Francesco OpenCode Apruzzese',
+		'DeveloperSite' => 'http://e-ware.org',
+		'DeveloperMail' => 'cescoap@gmail.com',
+		'SoftwareName' => 'PHPersonalBookmarks',
+		'CodeFormatLanguage' => array('PHP', 'JQuery', 'HTML5', 'CSS3'),
+		'LinkListFile' => 'link.list',
+	);
+
+?>
